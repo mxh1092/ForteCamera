ef273c40baae7f13d28434b3a5ebf850f21f6802
gate <mxh1092>
2021-12-02 10:18:18 +0100

    Added to readme

--- batches.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sbatch runForte.bash 1 10 0 0 0 387 test1
-sbatch runForte.bash 1 20 0 0 0 387 test2
-sbatch runForte.bash 1 30 0 0 0 387 test3
-sbatch runForte.bash 1 40 0 0 0 387 test4
-sbatch runForte.bash 1 50 0 0 0 387 test5
-
